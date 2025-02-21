95ef38acd202052a4a468d00353a08111f90c5fe
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-21 23:29:53 +0700

    API

---
+++ src/routes/v1/boardRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+
+
+const Router = express.Router()
+
+Router.route('/')
+  .get((req, res) => {
+    res.status(StatusCodes.OK).json({ message: 'API get lists board' })
+  })
+  .post((req, res) => {
+    res.status(StatusCodes.CREATED).json({ message: 'API create board' })
+  })
+
+export const boardRoutes = Router
