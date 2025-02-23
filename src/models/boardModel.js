4add0a0e4e6a30a192adfdcaba2793d91329bb31
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-23 12:26:01 +0700

    API Create Board

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,7 @@
 import Joi from 'joi'
+import { ObjectId } from 'mongodb'
+import { GET_DB } from '~/config/mongodb'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 // Define Collection ( Name & Schema )
 
@@ -7,13 +10,34 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   title: Joi.string().required().min(3).max(50).trim().strict(),
   slug: Joi.string().required().min(3).trim().strict(),
   description: Joi.string().required().min(3).max(256).trim().strict(),
-  columnOrderIds: Joi.array().items(Joi.string()).default([]),
+  columnOrderIds: Joi.array().items(
+    Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+const createBoard =async (data) => {
+  try {
+    const validDate = await validateBeforeCreate(data)
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(validDate)
+  } catch (error) { throw new Error(error)}
+}
+
+const findOneById =async (id) => {
+  try {
+    return await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
+      _id: new ObjectId(id)
+    })
+  } catch (error) { throw new Error(error)}
+}
+
 export const boardModel = {
   BOARD_COLLECTION_NAME,
-  BOARD_COLLECTION_SCHEMA
+  BOARD_COLLECTION_SCHEMA,
+  createBoard,
+  findOneById
 }
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,17 +1,21 @@
 /* eslint-disable no-useless-catch */
+import { boardModel } from '~/models/boardModel'
 import { slugify } from '~/utils/formatters'
-const newCreate = (reqBody) => {
+const newCreate = async (reqBody) => {
   try {
     const newBoard = {
       ...reqBody,
       slug: slugify(reqBody.title)
     }
+    const createBoard = await boardModel.createBoard(newBoard)
 
-    return newBoard
+    const getNewBoard = await boardModel.findOneById(createBoard.insertedId)
+    return getNewBoard
   } catch (error) {
     throw error
   }
 }
+
 
 export const boardService = {
   newCreate
