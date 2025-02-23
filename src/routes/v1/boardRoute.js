e1a2f11087b56ff36c88a9a7650e035069b5aadf
tuantran47 <21110810@student.hcmute.edu.vn>
2025-02-23 13:34:36 +0700

    API Get Details Board

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -11,4 +11,7 @@ Router.route('/')
   })
   .post( boardValidation.createNew, boardController.newCreate )
 
+Router.route('/:id')
+  .get(boardController.getDetails)
+  .put()
 export const boardRoute = Router
--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-useless-catch */
+import { StatusCodes } from 'http-status-codes'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
 import { slugify } from '~/utils/formatters'
 const newCreate = async (reqBody) => {
   try {
@@ -16,7 +18,19 @@ const newCreate = async (reqBody) => {
   }
 }
 
+const getDetails = async (boardId) => {
+  try {
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found!')
+    }
+    return board
+  } catch (error) {
+    throw new Error(error)
+  }
+}
 
 export const boardService = {
-  newCreate
+  newCreate,
+  getDetails
 }
